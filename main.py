802c9ea004a473b800eefdc0bf1eccdb39574ce7
aaronaubs <aaubs4@gmail.com>
2025-08-17 22:11:25 -0400

    Shorten greeting text and code handling user input
    Cleaned up code block which handles user_input,
    enhancing code readability and comprehension

--- main.py
+++ main.py
@@ -3,27 +3,22 @@ import login
 
 def greet_client():
     """Shows greeting message to user"""
-    print("*" * 40)
+    print("*" * 20)
     print("Welcome to CLI Bank!")
+    print("*" * 20)
     
 
 if __name__ == "__main__":
     greet_client()
-    user_input = login.check_new_client()
-    if user_input == "yes":
-        print("\nWelcome back!")
-        print("Please enter your username and password to log in")
-        username, user_id = login.get_username()
-        password = login.get_password(user_id)
+    user_input = login.request_login_or_signup()
+    if user_input == "login" or user_input == "log in":
+        login.attempt_login()
         print("Main Menu")
 
-    elif user_input == "no":
-        print("")
+    elif user_input == "signup" or user_input == "sign up":
         user_id = u.set_id()
         u.set_client_info(user_id)
         print("\nYour account is all set up!")
         print("*" * 40)
-        print("\nPlease enter your username and password to log in")
-        username, _ = login.get_username()
-        password = login.get_password(user_id)
+        login.attempt_login()
         print("Main Menu")
